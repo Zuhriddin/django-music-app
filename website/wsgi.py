b1826df2fe220431b09c1cb293070d738b2cb42c
Zuhriddin <Zuhriddin@users.noreply.github.com>
2017-09-16 20:14:00 +0900

    Settings:

--- website/wsgi.py
+++ website/wsgi.py
@@ -14,8 +14,9 @@ https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
 # os.environ.setdefault("DJANGO_SETTINGS_MODULE", "website.settings")
 #
 # application = get_wsgi_application()
+import os
 from django.core.wsgi import get_wsgi_application
 from whitenoise.django import DjangoWhiteNoise
-
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "website.settings")
 application = get_wsgi_application()
 application = DjangoWhiteNoise(application)
